9bd096e57602b5c5276cd65c4e7feaf3328c273b
Haira Lei <darhaii13@gmail.com>
2022-11-07 20:32:55 +0800

    add basic routes

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const tasks = require('./routes/tasks');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Home Page');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
