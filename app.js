333ea8121c87cb28b0617f183eb09c8d2fe95b6e
Haira Lei <darhaii13@gmail.com>
2022-11-09 15:23:02 +0800

    add error handler function and class. finish project

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // middlewares
 app.use(express.static('./public'));
